02d93fcb22cd2bfc0ec3b6be5ba1eb8c7161845e
cheesycod <cheesycod@outlook.com>
2023-08-05 12:37:17 +0530

    .

--- src/lib/components/SponsorPage.svelte
+++ src/lib/components/SponsorPage.svelte
@@ -9,16 +9,18 @@
 <article>
 	<Group title="Platinum Sponsors" id="platinum-sponsor">
 		<FlexedArea>
-			<Sponsor id="thebodyshop" name="The Body Shop" href="https://www.thebodyshop.in/?referrer=vivum" />
-			<Sponsor id="thirdwavecofee" name="Third Wave Coffee" href="https://www.thirdwavecoffeeroasters.com/?referrer=vivum" />
-			<Sponsor id="nivia" name="Nivia" href="https://www.niviasports.com/?referrer=vivum" />
+			<Sponsor id="thebodyshop" name="The Body Shop" href="https://www.thebodyshop.in/" />
+			<Sponsor id="thirdwavecofee" name="Third Wave Coffee" href="https://www.thirdwavecoffeeroasters.com/" />
+			<Sponsor id="nivia" name="Nivia" href="https://www.niviasports.com/" />
 		</FlexedArea>
 	</Group>
 	<Group title="Gold Sponsors" id="gold-sponsor">
 		<FlexedArea>
-			<Sponsor id="dclp" name="DCLP" href="" />
-			<Sponsor id="shingora" name="Shingora" href="https://shingora.net/?referrer=vivum" allowBlack={true} />
+			<Sponsor id="ion" name="I-ON" href="" />
+			<Sponsor id="shingora" name="Shingora" href="https://shingora.net/" allowBlack={true} />
 			<Sponsor id="champions-group" name="Champions Group" href="https://www.championsgroup.com/" />
+			<Sponsor id="rawpressery" name="Raw Pressery" href="https://www.rawpressery.com/" />
+			<Sponsor id="dailyhunt" name="DailyHunt" href="https://m.dailyhunt.in/" />
 		</FlexedArea>	
 	</Group>
 	<Group title="Silver Sponsors" id="silver-sponsor">
@@ -26,7 +28,7 @@
 			<Sponsor id="kangaro" name="Kangaro" href="https://kangarokgoc.com/" />
 			<Sponsor id="impelsys" name="Impelsys" href="https://www.impelsys.com/" />
 			<Sponsor id="rajsuperwhite" name="Raj Super White" href="http://www.rajsuperwhite.com/" />
-			<Sponsor id="redrhino" name="Redrhino" href="https://redrhino.co.in/" />
+			<Sponsor id="redrhino" name="Red Rhino" href="https://redrhino.co.in/" />
 
 		</FlexedArea>
 	</Group>
