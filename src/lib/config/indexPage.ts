8a1bda15ba1debc8ed92b4c98112b6e775200854
cheesycod <cheesycod@outlook.com>
2023-08-06 23:38:56 +0530

    fixed

--- src/lib/config/indexPage.ts
+++ src/lib/config/indexPage.ts
@@ -2,4 +2,4 @@ export const dates = '10th-11th August 2023';
 export const time = '9:00AM - 5:00PM';
 
 export const vivumStartDate = 'August 10 2023';
-export const email = "vivum2023@tisb.ac.in"
+export const email = "vivum23@tisb.ac.in"
